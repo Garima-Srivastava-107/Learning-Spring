716d6514843dd583654e8ea7cc99282dea14884d
Garima-Srivastava-107 <93173021+Garima-Srivastava-107@users.noreply.github.com>
2024-01-02 20:14:43 +0530

    Update Traveller.java

--- Traveller.java
+++ Traveller.java
@@ -1,7 +1,8 @@
 package org.example;
 
 public class Traveller {
-    //Suppose we want to change the medium through which the traveller will travel then we have to change the
+    //Suppose we want to change the medium through which the traveller will travel then we have to change then we have to change this entire traveller class then 
+    // to avoid this we will use the concept of loose coupling or dependency injection with the help of the interface
     Car car=null;
     Bus bus=null;
     Traveller(){
